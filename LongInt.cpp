e19885da28fdc236283811d17db26ddfa5d8d905
calvuS42 <olegka1302@gmail.com>
2017-10-25 11:12:14 +0300

    started

--- LongInt.cpp
+++ LongInt.cpp
@@ -5,3 +5,41 @@
 LongInt::LongInt()
 {
 }
+
+LongInt::LongInt(string)
+{
+}
+
+LongInt::LongInt(LongInt &)
+{
+}
+
+const LongInt operator+(LongInt a, LongInt b)
+{
+	return LongInt();
+}
+
+const LongInt operator-(LongInt a, LongInt b)
+{
+	return LongInt();
+}
+
+const LongInt operator*(LongInt a, LongInt b)
+{
+	return LongInt();
+}
+
+const LongInt operator/(LongInt a, LongInt b)
+{
+	return LongInt();
+}
+
+ostream & operator<<(ostream & stream, LongInt a)
+{
+	// TODO: insert return statement here
+}
+
+ostream & operator>>(ostream & stream, LongInt a)
+{
+	// TODO: insert return statement here
+}
--- LongInt.h
+++ LongInt.h
@@ -1,7 +1,28 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
 class LongInt
 {
 public:
 	LongInt();
+	LongInt(string);
+	LongInt(LongInt&);
+	friend const LongInt operator+ (LongInt a, LongInt b);
+	friend const LongInt operator- (LongInt a, LongInt b);
+	friend const LongInt operator* (LongInt a, LongInt b);
+	friend const LongInt operator/ (LongInt a, LongInt b);
+	//friend const LongInt operator= (LongInt a, LongInt b);
+	friend ostream& operator << (ostream& stream, LongInt a);
+	friend ostream& operator >> (ostream& stream, LongInt a);
+
+private:
+	vector <int> Number;
+	int base;
+	int n;
 };
 
+	
